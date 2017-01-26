e6afc9ada8e7d488d32a4ae61b3866ef158d7036
Владимир Калмыков <morulus@MacBook-Pro-Vladimir.local>
2017-01-27 02:22:12 +0300

    catch promise and inline errors

--- __tests__/singular.errors.js
+++ __tests__/singular.errors.js
@@ -16,18 +16,77 @@ describe('Throw error', () => {
     });
   });
 
-  it ('Returned error must be resolved', () => {
+  it ('Promise Try / catch | catched', () => {
+    function deliberateError() {
+      return Promise.reject(new Error("Oups"));
+    }
     function* testChain() {
-      let a = yield 1;
-      let b = yield new Error("Hello");
-      yield b.message;
+      try {
+        yield deliberateError;
+      } catch(e) {
+        yield 'Error catched';
+      }
     }
     return singular(testChain)
     .then(function(v) {
-      expect("Hello").toBe(v);
-    })
-    .catch(function(v) {
-      expect(false).toBe(true);
+      expect(v).toBe('Error catched');
+    });
+  });
+
+  it ('Promise Try / catch | uncatched', () => {
+    const handler = jest.fn((v) => {
+      expect(v.message).toBe("Oups");
+      return true;
+    });
+    function deliberateError() {
+      return Promise.reject(new Error("Oups"));
+    }
+    function* testChain() {
+      yield deliberateError;
+      yield 'Error catched';
+    }
+    return singular(testChain)
+    .catch(handler)
+    .then(() => {
+      expect(handler).toHaveBeenCalled();
+    });
+  });
+
+  it ('Inline throw Try / catch | catched', () => {
+    function deliberateError() {
+      return Promise.reject(new Error("Oups"));
+    }
+    function* testChain() {
+      try {
+        throw new Error("Inline error");
+        yield 7;
+      } catch(e) {
+        yield 'Error catched';
+      }
+    }
+    return singular(testChain)
+    .then(function(v) {
+      expect(v).toBe('Error catched');
+    });
+  });
+
+  it ('Inline throw Try / catch | uncatched', () => {
+    const handler = jest.fn((v) => {
+      expect(v.message).toBe("Inline error");
+      return true;
+    });
+    function deliberateError() {
+      return Promise.reject(new Error("Oups"));
+    }
+    function* testChain() {
+      throw new Error("Inline error");
+      yield 7;
+      yield 'Error catched';
+    }
+    return singular(testChain)
+    .catch(handler)
+    .then(() => {
+      expect(handler).toHaveBeenCalled();
     });
   });
 });
