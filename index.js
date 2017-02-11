062fd002065d6986ee45a9742ddf619a54148297
Vladimir Kalmykov <v.kalmykov@infotech.team>
2017-02-11 20:02:50 +0300

    export cancel and cancelled

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
 exports.singular = require('./lib/singular.js');
 exports.MESSAGE = require('./lib/constants.js').MESSAGE;
 exports.RESTANTE = require('./lib/constants.js').RESTANTE;
+exports.CANCEL = require('./lib/constants.js').CANCEL;
+exports.CANCELLED = require('./lib/constants.js').CANCELLED;
